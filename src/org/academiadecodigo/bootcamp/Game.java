4a7c7a97992d26c30567f9bca04b409db4fad861
Nuno Amaral <n20amaral@outlook.com>
2018-10-05 23:53:02 +0000

    added intial class structure

--- src/org/academiadecodigo/bootcamp/Game.java
+++ src/org/academiadecodigo/bootcamp/Game.java
@@ -1,24 +1,26 @@
 package org.academiadecodigo.bootcamp;
 
+import org.academiadecodigo.bootcamp.gameobjects.Grid;
 import org.academiadecodigo.simplegraphics.graphics.Color;
 import org.academiadecodigo.simplegraphics.graphics.Rectangle;
 
 public class Game {
     public static final int PADDING = 10;
-    public static final int TILE_SIZE = 50;
-
+    public static final int BRICK_WIDTH = 50;
+    public static final int BRICK_HEIGHT = 50;
 
     private Rectangle canvas = new Rectangle(PADDING, PADDING, 800, 600);
-    private Rectangle tileGrid;
+    private Grid beltGrid;
+    private Grid carrierGrid;
+    private Grid stackGrid;
+     
 
     public Game(int cols, int rows) {
-        tileGrid = new Rectangle(PADDING, PADDING, cols * TILE_SIZE, rows * TILE_SIZE);
+
     }
 
     public void init() {
         canvas.draw();
-        tileGrid.setColor(Color.LIGHT_GRAY);
-        tileGrid.fill();
     }
 
     public void start() {
