37010c1470555e94628bf82f7d10ce3543a54c2c
Nuno Amaral <n20amaral@outlook.com>
2018-10-05 15:28:42 +0000

    canvas created

---
+++ src/org/academiadecodigo/bootcamp/Game.java
@@ -0,0 +1,13 @@
+package org.academiadecodigo.bootcamp;
+
+import org.academiadecodigo.simplegraphics.graphics.Rectangle;
+
+public class Game {
+    public static final int PADDING = 10;
+
+    private Rectangle canvas = new Rectangle(PADDING, PADDING, 800, 600);
+
+    public void init() {
+        canvas.draw();
+    }
+}
---
+++ src/org/academiadecodigo/bootcamp/Stax.java
@@ -0,0 +1,8 @@
+package org.academiadecodigo.bootcamp;
+
+public class Stax {
+    public static void main(String[] args) {
+        Game stax = new Game();
+        stax.init();
+    }
+}
