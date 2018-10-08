ac296f0288fd228ed27a351d34303b23fe08e158
João Quadros <joaoquadros20140396@gmail.com>
2018-10-08 20:15:00 +0000

    belt grid structured

--- src/org/academiadecodigo/bootcamp/gameobjects/grid/BeltGrid.java
+++ src/org/academiadecodigo/bootcamp/gameobjects/grid/BeltGrid.java
@@ -1,9 +1,26 @@
 package org.academiadecodigo.bootcamp.gameobjects.grid;
 
-import org.academiadecodigo.simplegraphics.graphics.Color;
+import org.academiadecodigo.bootcamp.gameobjects.brick.Brick;
 
 public class BeltGrid extends Grid {
-    public BeltGrid(int cols, int rows, Color color) {
-        super(cols, rows, color);
+
+    public BeltGrid(int cols, int rows) {
+        super(cols, rows, rows);
     }
+
+    public void addNewBrick() {
+
+    }
+
+    public void moveAllBricks() {
+
+    }
+
+    public Brick getFallingBrick() {
+        return new Brick();
+    }
+
+    
+
+
 }
