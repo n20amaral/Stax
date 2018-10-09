02b614465a3c4e4418c153270b7058a3cc92824c
Nuno Amaral <n20amaral@outlook.com>
2018-10-09 21:55:49 +0000

    implement Displayable on Grid

--- src/org/academiadecodigo/bootcamp/gameobjects/grid/Grid.java
+++ src/org/academiadecodigo/bootcamp/gameobjects/grid/Grid.java
@@ -1,14 +1,16 @@
 package org.academiadecodigo.bootcamp.gameobjects.grid;
 
 import org.academiadecodigo.bootcamp.Game;
+import org.academiadecodigo.bootcamp.gameobjects.Displayable;
 import org.academiadecodigo.bootcamp.gameobjects.brick.Brick;
 import org.academiadecodigo.simplegraphics.graphics.Color;
 import org.academiadecodigo.simplegraphics.graphics.Rectangle;
 
-public abstract class Grid {
+public abstract class Grid implements Displayable {
     private int cols;
     private int rows;
     protected Brick[] bricks;
+    private Rectangle rectangle;
 
     public Grid(int cols, int rows, int maxBricks) {
         this.cols = cols;
@@ -25,4 +27,39 @@ public abstract class Grid {
     }
 
 
+    @Override
+    public void show(int x, int y, Color color) {
+        if (rectangle == null) {
+            rectangle = new Rectangle(x, y, cols * Game.BRICK_WIDTH, rows * Game.BRICK_HEIGHT);
+        }
+
+        rectangle.setColor(color);
+        rectangle.fill();
+
+    }
+
+    @Override
+    public void hide() {
+        rectangle.delete();
+    }
+
+    @Override
+    public int getX() {
+        return rectangle.getX();
+    }
+
+    @Override
+    public int getY() {
+        return rectangle.getY();
+    }
+
+    @Override
+    public int getWidth() {
+        return rectangle.getWidth();
+    }
+
+    @Override
+    public int getHeight() {
+        return rectangle.getHeight();
+    }
 }
