c359ab7a06877cc9ccbb8a88cd65e25119bce7d5
Firmino <firmino_gxt@hotmail.com>
2018-10-09 22:19:31 +0000

    carrier implements Displayable

--- src/org/academiadecodigo/bootcamp/gameobjects/Carrier.java
+++ src/org/academiadecodigo/bootcamp/gameobjects/Carrier.java
@@ -6,39 +6,25 @@ import org.academiadecodigo.bootcamp.gameobjects.brick.*;
 import org.academiadecodigo.simplegraphics.graphics.*;
 import org.academiadecodigo.simplegraphics.keyboard.*;
 
-public class Carrier implements KeyboardHandler {
+public class Carrier implements Displayable, KeyboardHandler {
 
     private CarrierGrid grid;
- //   private Rectangle carrier;
+    private Rectangle carrier;
+    private Color color = Color.DARK_GRAY;
     private Brick brick;
     private int col;
     private int row;
     private Keyboard keyboard;
 
-    public Carrier(CarrierGrid grid) {
-        this.col = 1;
-        this.row = 1;
+    public Carrier(int col, int row, CarrierGrid grid) {
+        this.col = col;
+        this.row = row;
         this.grid = grid;
-  /*      this.carrier = new Rectangle(grid.getX() + col * Game.BRICK_WIDTH,grid.getY() +  row * Game.BRICK_HEIGHT, Game.BRICK_WIDTH,Game.BRICK_HEIGHT);
-        carrier.setColor(Color.CYAN);
-        this.keyboard = new Keyboard(this);
-        show(); */
     }
 
     public int getCol() {
         return col;
     }
-
-  /*  private void show() {
-        carrier.fill();
-        if (brick != null){
-            //brick.fill();
-        }
-    }
-
-    private void hide() {
-        carrier.delete();
-    } */
 
     public void addBrick(Brick brick) {
         if (brick.getCol() == col && this.brick != null) {
@@ -48,7 +34,8 @@ public class Carrier implements KeyboardHandler {
         }
     }
 
-    private void releaseBrick() { // implement buffer here
+    private void releaseBrick() {
+
         if (brick != null) {
             grid.addReleasedBrick(brick);
             brick = null;
@@ -56,6 +43,8 @@ public class Carrier implements KeyboardHandler {
     }
 
     public void init() {
+
+        this.keyboard = new Keyboard(this);
 
         KeyboardEvent left = new KeyboardEvent();
         KeyboardEvent right = new KeyboardEvent();
@@ -77,25 +66,23 @@ public class Carrier implements KeyboardHandler {
 
     public void moveLeft() {
         if (col > 0) {
-            //hide();
             col--;
             if (brick != null) {
                 brick.setCol(col);
             }
             //show();
-           // carrier.translate(0 - Game.BRICK_WIDTH, 0);
+            // carrier.translate(0 - Game.BRICK_WIDTH, 0);
         }
 
     }
 
     public void moveRight() {
-        if (col < grid.getCols()-1) {
-            //hide();
+        if (col < grid.getCols() - 1) {
             col++;
             if (brick != null) {
                 brick.setCol(col);
             }
-           // carrier.translate(Game.BRICK_WIDTH, 0);
+            // carrier.translate(Game.BRICK_WIDTH, 0);
             //show();
         }
 
@@ -122,5 +109,39 @@ public class Carrier implements KeyboardHandler {
     @Override
     public void keyReleased(KeyboardEvent keyboardEvent) {
 
+    }
+
+    @Override
+    public void show(int x, int y) {
+        if (carrier == null) {
+            carrier = new Rectangle(x, y, Game.BRICK_WIDTH, Game.BRICK_HEIGHT);
+            carrier.setColor(color);
+        }
+        carrier.fill();
+    }
+
+    @Override
+    public void hide() {
+        carrier.delete();
+    }
+
+    @Override
+    public int getX() {
+        return carrier.getX();
+    }
+
+    @Override
+    public int getY() {
+        return carrier.getY();
+    }
+
+    @Override
+    public int getWidth() {
+        return carrier.getWidth();
+    }
+
+    @Override
+    public int getHeight() {
+        return carrier.getHeight();
     }
 }
