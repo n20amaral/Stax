27709dc9ec56900c23eb8f1a4cc8d1b2e724f804
Joao Narra <joaonarra23@gmail.com>
2018-10-11 19:37:35 +0000

    Added lives

--- src/org/academiadecodigo/bootcamp/Game.java
+++ src/org/academiadecodigo/bootcamp/Game.java
@@ -15,7 +15,7 @@ public class Game {
     public static final int BRICK_HEIGHT = 25;
     public static final int GRID_PADDING = (CANVAS_WIDTH - Stax.MAX_COLS * BRICK_WIDTH) / 2 + PADDING;
 
-
+    private int lives = 5;
     private int cols;
     private int score = 0;
     private Rectangle canvas = new Rectangle(PADDING, PADDING, CANVAS_WIDTH, CANVAS_HEIGHT);
@@ -26,7 +26,8 @@ public class Game {
     private Brick brick;
     private boolean gameOver;
     private int step = 5;
-    private Text textScore = new Text(20,20,"Score: " + score);
+    private Text textScore = new Text(30,20,"Score: " + score);
+    private Text textLives = new Text(30, 60,"Lives: " + lives);
 
     public Game(int cols) {
         this.cols = cols;
@@ -40,8 +41,10 @@ public class Game {
 
 
         canvas.draw();
-        textScore.grow(10,10);
+        textScore.grow(20,20);
         textScore.draw();
+        textLives.grow(20,20);
+        textLives.draw();
         beltGrid.show(GRID_PADDING,PADDING);
         carrierGrid.show(GRID_PADDING,PADDING + beltGrid.getHeight());
         stackGrid.show(GRID_PADDING,PADDING + beltGrid.getHeight() + carrierGrid.getHeight());
@@ -90,6 +93,13 @@ public class Game {
 
         if (!carrier.addBrick(brick)){
             brick.hide();
+            this.lives--;
+            textLives.setText("Lives: " + lives);
+            textLives.draw();
+
+            if (lives == 0) {
+                endgame();
+            }
         }
     }
 
